d18dd2cc292c1aaf42d7d2c8b4a53660f48c21ff
szubing <szubing@qq.com>
2023-05-19 08:19:08 +0000

    delected unrelated codes

--- datasets/utils.py
+++ datasets/utils.py
@@ -6,12 +6,6 @@ from datasets import dataset_classes
 from tools.utils import load_json
 
 
-def get_few_shot_setup_name(train_shot, seed):
-    """Get the name for a few-shot setup.
-    """
-    return f"shot_{train_shot}-seed_{seed}"
-
-
 class TextTensorDataset(torch.utils.data.Dataset):
     def __init__(self, input_tensor, label_tensor, eot_indices):
         self.input_tensor = input_tensor
@@ -85,40 +79,3 @@ class FeatureWrapper(torch.utils.data.Dataset):
         }
 
         return output
-
-
-def get_few_shot_benchmark(data_dir,
-                           indices_dir,
-                           dataset,
-                           train_shot,
-                           seed):
-    # Check if the dataset is supported
-    assert dataset in dataset_classes
-    few_shot_index_file = os.path.join(
-        indices_dir, dataset, f"{get_few_shot_setup_name(train_shot, seed)}.json")
-    assert os.path.exists(few_shot_index_file), f"Few-shot data does not exist at {few_shot_index_file}."
-    benchmark = dataset_classes[dataset](data_dir)
-    few_shot_dataset = load_json(few_shot_index_file)
-    return {
-        'train': few_shot_dataset['train']['data'],
-        'val': few_shot_dataset['val']['data'],
-        'test': benchmark.test,
-        'lab2cname': benchmark.lab2cname,
-        'classnames': benchmark.classnames,
-    }
-
-
-def get_testset(dataset, data_dir):
-    if dataset in dataset_classes:
-        benchmark = dataset_classes[dataset](data_dir)
-        return benchmark.test
-    else:
-        raise NotImplementedError()
-
-
-def get_label_map(data_dir, dataset_name):
-    if dataset_name in ['imagenet_a', 'imagenet_r']:
-        benchmark = dataset_classes[dataset_name](data_dir)
-        return benchmark.label_map
-    else:
-        return None
